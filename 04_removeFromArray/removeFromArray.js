5c9b75f374aa871bb36ae6d2b0d62f5f263cf527
cwymbewe <cwymbewe@gmail.com>
2023-10-15 00:37:45 +0200

    Edit removeFromArray .js and .spec.js files

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let newArr = [];
+    let argsArr = [...args];
+    for(let i = 0; i < arr.length; i++){
+        if(!argsArr.includes(arr[i])){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
