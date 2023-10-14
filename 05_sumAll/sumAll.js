2628c5117795fed1418aba1d4abeb99a6c25e469
cwymbewe <cwymbewe@gmail.com>
2023-10-15 01:56:59 +0200

    Edit sumAll .js and .spec.js files

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+let sum = 0;
+    if(min < 0 || max < 0) { return `ERROR`};
 
+    if(min < max && min > 0){
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+    }else if(min > max && max > 0){
+        for (let j = max; j <= min ; j++) {
+            sum += j;
+        }
+    }
+
+    if(typeof min !== 'number' || typeof max !== 'number') {
+        return "ERROR";
+    }
+
+return sum;
 };
 
 // Do not edit below this line
